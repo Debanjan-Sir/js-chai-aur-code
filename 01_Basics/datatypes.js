a73ea113ffbb31bb8693482073573ac4d13c3896
Debanjan Mazumder <debanjanmazumder0310@gmail.com>
2026-05-26 04:45:52 +0530

    datatypes learned

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" ; //treats all the code as modern and secure
+
+// alert("Welcome to JavaScript"); // we are using nodejs so alert will not work here
+
+console.log("Welcome to JavaScript"); // this is how we print something in console
+
+// Data types in JavaScript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol (ES6)
+// 7. BigInt (ES2020)
+
+console.log(typeof null); // object (this is a known bug in JavaScript)
+console.log(typeof undefined); // undefined
